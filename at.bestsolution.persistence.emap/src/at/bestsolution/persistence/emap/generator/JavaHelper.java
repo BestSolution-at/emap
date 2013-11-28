3733f92ea26e6137679850370a13beeda9438ba2
Tom Schindl <tom.schindl@bestsolution.at>
2013-11-28 11:10:17 +0100

    load ecore from workspace/target

--- at.bestsolution.persistence.emap/src/at/bestsolution/persistence/emap/generator/JavaHelper.java
+++ at.bestsolution.persistence.emap/src/at/bestsolution/persistence/emap/generator/JavaHelper.java
@@ -2,16 +2,44 @@ package at.bestsolution.persistence.emap.generator;
 
 import java.util.Map;
 
+import org.eclipse.emf.codegen.ecore.genmodel.GenClassifier;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
+import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 import at.bestsolution.persistence.emap.eMap.EType;
 
 public class JavaHelper {
 
 	public static EClass getEClass(EType type) {
+		// Look in the local workspace first
+		Map<String, URI> map = EcorePlugin.getEPackageNsURIToGenModelLocationMap(true);
+		
+		URI uri = map.get(type.getUrl());
+		
+		if( uri != null ) {
+			ResourceSet s = new ResourceSetImpl();
+			Resource resource = s.getResource(uri, true);
+			GenModel m = (GenModel) resource.getContents().get(0);
+			for( GenPackage gp : m.getAllGenPackagesWithClassifiers() ) {
+				if( gp.getNSURI().equals(type.getUrl()) ) {
+					EClass e = (EClass) gp.getEcorePackage().getEClassifier(type.getName());
+					for( GenClassifier c : gp.getGenClassifiers() ) {
+						if( c.getName().equals(e.getName()) ) {
+							e.setInstanceClassName(c.getImportedInstanceClassName());
+							return e;
+						}
+					}
+				}
+			}
+		}
+		
 		EPackage ePackage = EPackage.Registry.INSTANCE.getEPackage(type.getUrl());
 		if( ePackage == null ) {
 			throw new IllegalStateException("Unknown package '"+type.getUrl()+"'");
@@ -25,4 +53,26 @@ public class JavaHelper {
 
 		return eClass;
 	}
+	
+//	private static GenClass getGenClass(EClass eClass) {
+//		Map<String, URI> map = EcorePlugin.getEPackageNsURIToGenModelLocationMap(true);
+//		URI uri = map.get(eClass.getEPackage().getNsURI());
+//		ResourceSet s = new ResourceSetImpl();
+//		GenModel m = (GenModel) s.getResource(uri, true).getContents().get(0);
+//		for( GenPackage p : m.getAllGenPackagesWithClassifiers() ) {
+//			for( GenClassifier gc : p.getGenClassifiers() ) {
+//				if( gc.getName().equals(eClass.getName()) ) {
+//					return (GenClass) gc;
+//				}
+//			}
+//		}
+//		return null;
+//	}
+//	
+//	public static String calcInstanceClassName(EClass eClass) {
+//		String rv = eClass.getInstanceClassName();
+//		if( rv == null ) {
+//			getGenClass(eClass);
+//		}
+//	}
 }
