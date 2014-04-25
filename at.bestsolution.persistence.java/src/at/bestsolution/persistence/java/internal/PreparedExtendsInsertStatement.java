664d256485d9a4024fa1f3073381244c8567abb1
Christoph Caks <ccaks@bestsolution.at>
2014-04-25 11:34:49 +0200

    implemented insert statements for entities using extends

---
+++ at.bestsolution.persistence.java/src/at/bestsolution/persistence/java/internal/PreparedExtendsInsertStatement.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christoph Caks <christoph.caks@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package at.bestsolution.persistence.java.internal;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import at.bestsolution.persistence.java.DatabaseSupport.ExtendsInsertStatement;
+import at.bestsolution.persistence.java.DatabaseSupport.InsertStatement;
+
+public class PreparedExtendsInsertStatement extends PreparedStatement implements ExtendsInsertStatement {
+	private final String tableName;
+	private final String pkColumn;
+	
+	static final Logger LOGGER = Logger.getLogger(PreparedStatement.class);
+	
+	public PreparedExtendsInsertStatement(String tableName, String pkColumn) {
+		this.tableName = tableName;
+		this.pkColumn = pkColumn;
+	}
+	
+	protected String createSQL(String tableName, String pkColumn, long pkValue, List<Column> columnList) {
+		StringBuilder col = new StringBuilder();
+		StringBuilder val = new StringBuilder();
+
+		if (pkColumn != null && pkValue != 0) {
+			col.append('"'+pkColumn+'"');
+			val.append(pkValue);
+		}
+
+		for (Column c : columnList) {
+			if (col.length() != 0) {
+				col.append("\n,");
+				val.append("\n,");
+			}
+			col.append('"'+c.column+'"');
+			val.append("?");
+		}
+
+		return "INSERT INTO "+'"' + tableName + '"' +"(" + col + ") VALUES (" + val + ")";
+	}
+	
+	@Override
+	public final boolean execute(Connection connection, long primaryKeyValue) throws SQLException {
+		final String sql = createSQL(tableName, pkColumn, primaryKeyValue, columnList);
+		LOGGER.debug("Executing statement '"+sql+"'");
+		final java.sql.PreparedStatement pstmt = connection.prepareStatement(sql);
+		
+		for( final Column c : columnList ) {
+			c.apply(pstmt);
+		}
+		
+		try {
+			return pstmt.execute();	
+		} finally {
+			pstmt.close();
+		}
+	}
+}
