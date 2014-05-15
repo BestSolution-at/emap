a6f3a053b111622ae8d46bba90a7bf05dfab62d8
Tom Schindl <tom.schindl@bestsolution.at>
2014-05-15 12:36:09 +0200

    add a function interface

---
+++ at.bestsolution.persistence/src/at/bestsolution/persistence/Function.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2014 BestSolution.at and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     tom <FIRSTNAME.LASTNAME@bestsolution.at> - initial API and implementation
+ *******************************************************************************/
+package at.bestsolution.persistence;
+
+public interface Function<T> {
+	public void execute(T value);
+}
