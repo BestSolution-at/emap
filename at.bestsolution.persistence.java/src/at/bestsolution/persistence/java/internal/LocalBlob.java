b0bc956fb39aab204460556eb90d5be0668ad82c
Tom Schindl <tom.schindl@bestsolution.at>
2014-07-09 17:01:09 +0200

    fixed blog

--- at.bestsolution.persistence.java/src/at/bestsolution/persistence/java/internal/LocalBlob.java
+++ at.bestsolution.persistence.java/src/at/bestsolution/persistence/java/internal/LocalBlob.java
@@ -23,31 +23,31 @@ import java.sql.SQLFeatureNotSupportedException;
 public class LocalBlob implements Blob {
 	private File storageFile;
 	private RandomAccessFile storage;
-	
+
 	private boolean blobInvalid = false;
-	
+
 	private void checkInvalid() throws SQLException {
 		if (blobInvalid) {
 			throw new SQLException("BLOB invalid (already freed)");
 		}
 	}
-	
+
 	private void checkPointer(long pointer) throws SQLException {
 		if (pointer < 1) {
 			throw new SQLException("pointer must be >= 1");
 		}
 	}
-	
+
 	private File createStorageFile() throws IOException {
 		final File storageFile = File.createTempFile("emap", "blob");
 		storageFile.deleteOnExit();
 		return storageFile;
 	}
-	
+
 	private RandomAccessFile createStorage(File storageFile) throws IOException {
 		return  new RandomAccessFile(storageFile, "rw");
 	}
-	
+
 	private RandomAccessFile getStorage() throws IOException {
 		if (storage == null) {
 			storageFile = createStorageFile();
@@ -55,7 +55,7 @@ public class LocalBlob implements Blob {
 		}
 		return storage;
 	}
-	
+
 	@Override
 	protected void finalize() throws Throwable {
 		super.finalize();
@@ -73,7 +73,7 @@ public class LocalBlob implements Blob {
 			}
 		}
 	}
-	
+
 	@Override
 	public void free() throws SQLException {
 		try {
@@ -186,76 +186,76 @@ public class LocalBlob implements Blob {
 			throw new SQLException(e);
 		}
 	}
-	
+
 	static class RAF_OutputStream extends OutputStream {
 		private final RandomAccessFile raf;
 		private long offset;
-		
+
 		public RAF_OutputStream(RandomAccessFile raf) {
 			this(raf, 0);
 		}
-		
+
 		public RAF_OutputStream(RandomAccessFile raf, long offset) {
 			this.raf = raf;
 			this.offset = offset;
 		}
-		
+
 		@Override
 		public void write(byte[] b) throws IOException {
 			this.write(b, 0, b.length);
 		}
-		
+
 		@Override
 		public void write(byte[] b, int off, int len) throws IOException {
 			raf.seek(offset);
-			raf.write(b, off, len);
+			raf.write(b, (int)(off-offset), len);
 			offset += len;
 		}
-		
+
 		@Override
 		public void write(int b) throws IOException {
 			raf.seek(offset);
 			raf.write(b);
 			offset += 1;
 		}
-		
+
 	}
-	
+
 	static class RAF_InputStream extends InputStream {
 		private final RandomAccessFile raf;
 		private int offset;
-		
+
 		public RAF_InputStream(RandomAccessFile raf) {
 			this.raf = raf;
 			this.offset = 0;
 		}
-		
+
 		@Override
 		public int read(byte[] b) throws IOException {
 			return read(b, offset, b.length);
 		}
-		
+
 		@Override
 		public int read(byte[] b, int off, int len) throws IOException {
 			if (offset >= raf.length()) {
 				return -1;
 			}
-			
+
 			raf.seek(offset);
-			int readLen = raf.read(b, off, len);
+			int readLen = raf.read(b, off-offset, len);
 			if (readLen != -1) {
 				offset += readLen;
 			}
 			return readLen;
 		}
-		
-		
+
+
 		@Override
 		public int read() throws IOException {
 			if (offset >= raf.length()) {
 				return -1;
 			}
-			
+
 			raf.seek(offset);
 			int r = raf.read();
 			offset += 1;
