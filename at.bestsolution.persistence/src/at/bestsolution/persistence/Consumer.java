e1db33efaf961514f03e45b8d60ab8676fa707fe
Tom Schindl <tom.schindl@bestsolution.at>
2014-07-30 13:01:06 +0200

    renamed to match java8 types

--- at.bestsolution.persistence/src/at/bestsolution/persistence/Consumer.java
+++ at.bestsolution.persistence/src/at/bestsolution/persistence/Consumer.java
@@ -10,6 +10,6 @@
  *******************************************************************************/
 package at.bestsolution.persistence;
 
-public interface Function<T,E extends Throwable> {
+public interface Consumer<T,E extends Throwable> {
 	public void execute(T value) throws E;
 }
