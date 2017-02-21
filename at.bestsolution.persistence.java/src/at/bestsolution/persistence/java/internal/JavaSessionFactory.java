c2139d522ede85f8bd68ff3425e662381fd1d303
Christoph Caks <ccaks@bestsolution.at>
2017-02-21 12:48:15 +0100

    #40 fixed

--- at.bestsolution.persistence.java/src/at/bestsolution/persistence/java/internal/JavaSessionFactory.java
+++ at.bestsolution.persistence.java/src/at/bestsolution/persistence/java/internal/JavaSessionFactory.java
@@ -1209,7 +1209,7 @@ public class JavaSessionFactory implements SessionFactory {
 			for( RelationSQL r : list ) {
 				if( r.getAction() == sql.getAction() && r.getTableName().equals(sql.getTableName()) ) {
 					if( r.getSelf() == sql.getSelf() &&
-						r.getOpposite() == r.getOpposite()	) {
+						r.getOpposite() == sql.getOpposite()	) {
 						if( isDebug ) {
 							LOGGER.debug("Skiping registration because same is already registered");
 						}
